cdd24176b714fdc7915cdf324b8f4663a1539475
AsunderAgain <41303166+AsunderAgain@users.noreply.github.com>
2018-07-16 15:08:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+alert("Website for writers and aspiring writers only!");
+
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+ //remember to add semicolons at the end.
